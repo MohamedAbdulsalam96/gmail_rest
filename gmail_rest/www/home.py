dfe840c9e11b3baf273e26aac5ad81bb791466e6
fadilsid <fadil@tridz.com>
2023-03-06 08:40:16 +0000

    fix: fixes

--- gmail_rest/www/home.py
+++ gmail_rest/www/home.py
@@ -59,7 +59,8 @@ def authorize():
 
   # Store the state so the callback can verify the auth server response.
   google_credentials.state = state
-  google_credentials.insert()
+  google_credentials.insert(ignore_permissions=True)
+  google_credentials.save(ignore_permissions=True)
   frappe.local.response['type'] = 'redirect'
   frappe.local.response['location'] = authorization_url
 
@@ -89,7 +90,8 @@ def oauth2callback():
   cred=credentials_to_dict(credentials)
   google_credentials.token = cred['token']
   google_credentials.refresh_token = cred['refresh_token']
-  google_credentials.insert() 
+  google_credentials.insert(ignore_permissions=True)
+  google_credentials.save(ignore_permissions=True) 
   frappe.local.response['type'] = 'redirect'
   frappe.local.response['location'] = '/app'
   
