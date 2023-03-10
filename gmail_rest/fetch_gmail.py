1c7f33e18bb2f0fe3c9c33d48eec41c920cab782
fadilsid <fadil@tridz.com>
2023-03-10 12:28:03 +0000

    fix: minor fix

--- gmail_rest/fetch_gmail.py
+++ gmail_rest/fetch_gmail.py
@@ -43,7 +43,7 @@ def fetch():
     })
 
     delimiter=','
-    string=delimiter.join(str(i) for i in fetch_data)
+    string=delimiter.join(str(i) for i in threads)
 
     string.insert(ignore_permissions=True)
     
