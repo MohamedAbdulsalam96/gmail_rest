e34dccbf49764e93fc71a195ba6c80f317ae1574
“Fadil <fadil@tridz.com>
2023-04-05 19:16:11 +0000

    feat:coomon function to send email

---
+++ gmail_rest/gmail_rest/email/mail.py
@@ -0,0 +1,25 @@
+from gmail_rest.gmail_rest.email.gmail.send import gmail_send_message
+import frappe
+
+def sendmail(
+    doctype=None,
+    name=None,
+    recipients=None,
+    subject=None,
+    content=None,
+    cc=None,
+    bcc=None
+):
+    email_settings = frappe.get_doc('Email Credentials')
+    if email_settings.service_provider == 'gmail':
+        gmail_send_message(
+            doctype=doctype,
+            name=name,
+            recipients=name,
+            subject=subject,
+            content=content,
+            cc=cc,
+            bcc=bcc
+        )
+    elif email_settings.service_provider == None:
+        frappe.throw("Setup Email Account")
