680d0525773a9af652a7ce65a0646e09ae3f9752
“Fadil <fadil@tridz.com>
2023-04-17 20:24:59 +0000

    fix: message id

--- gmail_rest/gmail_rest/email/gmail/receive.py
+++ gmail_rest/gmail_rest/email/gmail/receive.py
@@ -33,7 +33,7 @@ def fetch():
     for thread in threads:
         thread_id = thread['id']
         thread_data = gmail.users().threads().get(userId='me', id=thread_id,format='full').execute()
-        thread_info.append(thread_data['messages'][-1])
+        thread_info.append(thread_data['messages'])
         message = thread_data['messages'][-1]
         payload = message['payload']
         headers = payload['headers']
