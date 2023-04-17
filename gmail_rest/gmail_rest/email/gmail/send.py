da0c2a15ee3a73949b1412d59894acb35b87e45f
“Fadil <fadil@tridz.com>
2023-04-17 21:00:39 +0000

    fix: replace hardcoded message-id

--- gmail_rest/gmail_rest/email/gmail/send.py
+++ gmail_rest/gmail_rest/email/gmail/send.py
@@ -88,7 +88,7 @@ def gmail_send_message(
         message['From'] = google_credentials.email
         message['Subject'] = subject
         message['Reply-To']=google_credentials.email
-        message['In-Reply-To']='<CALFBqx51f4G6bjNhj4MR9UT4xYMp8X5v4VQJadiUio=uoRpV8w@mail.gmail.com>'
+        message['In-Reply-To']=message_id
 
   
         threads = service.users().threads().list(
